52a30f8a0d54c79c001273ba6d0cffe527df64bb
KasterNM <drevenger7351@gmail.com>
2026-04-11 02:59:50 -0300

    Rewrite player script

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,50 +1,89 @@
 extends CharacterBody2D
 
 @export_category("Movement")
-@export_enum("Grid-based", "Platform") var movement_type : String = "Platform"
-@export var speed := 256.0
-@export var acceleration := 5.0
+@export_enum("Platform", "Grid-based") var movement_type : String = "Platform"
+@export var speed := 384.0
+@export var acceleration := 4.0
 @export var friction := 10.0
 @export var jump_strength := 464.0
 @export var gravity := 1536.0
 @export var max_fall_speed := 1024.0
-@export var godmode: bool = false
+@export var no_clip: bool = false:
+	set(value):
+		no_clip = value
+		collision_shape_2d.disabled = value
+
+@export var collision_shape_2d: CollisionShape2D
+
+
+func _process(delta: float) -> void:
+	if no_clip:
+		_no_clip_update(delta)
+	else:
+		match movement_type:
+			"Platform":
+				_platform_update(delta)
+			"Grid-based":
+				_grid_based_update(delta)
+
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("GODMODE"):
-		godmode = not godmode
+	if no_clip:
+		_no_clip_physics_update(delta)
+	else:
+		match movement_type:
+			"Platform":
+				_platform_physics_update(delta)
+			"Grid-based":
+				_grid_based_physics_update(delta)
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("NOCLIP"):
+		no_clip = not no_clip
+
+
+func _no_clip_update(_delta: float) -> void:
+	pass
+
+
+func _no_clip_physics_update(delta: float) -> void:
+	var direction := Input.get_vector("LEFT", "RIGHT", "UP", "DOWN")
 	
-	if godmode:
-		collision_mask = 0
-		var x_direction := Input.get_axis("LEFT", "RIGHT")
-		var y_direction := Input.get_axis("UP", "DOWN")
-		
-		if x_direction == 0:
-			velocity.x = lerp(velocity.x, 0.0, 4 * delta)
-		else:
-			velocity.x = lerp(velocity.x, 320 * x_direction, acceleration * delta)
-			
-		if y_direction == 0:
-			velocity.y = lerp(velocity.y, 0.0, 4 * delta)
-		else:
-			velocity.y = lerp(velocity.y, 320 * y_direction, acceleration * delta)
-			
-		move_and_slide()
-	elif movement_type == "Platform":
-		collision_mask = 1
-		var direction := Input.get_axis("LEFT", "RIGHT")
-		
-		if direction == 0:
-			velocity.x = lerp(velocity.x, 0.0, friction * delta)
-		else:
-			velocity.x = lerp(velocity.x, speed * direction, acceleration * delta)
-		
-		if not is_on_floor() and \
-		velocity.y < max_fall_speed:
-			velocity.y = min(velocity.y + gravity * delta, max_fall_speed)
-		
-		if Input.is_action_just_pressed("JUMP") and \
-		is_on_floor():
-			velocity.y = -jump_strength
-		
-		move_and_slide()
+	if direction == Vector2.ZERO:
+		velocity = lerp(velocity, Vector2.ZERO, friction * delta)
+	else:
+		velocity = lerp(velocity, speed * direction, acceleration * delta)
+	
+	move_and_slide()
+
+
+func _platform_update(_delta: float) -> void:
+	pass
+
+
+func _platform_physics_update(delta: float) -> void:
+	var direction := Input.get_axis("LEFT", "RIGHT")
+	
+	if direction == 0:
+		velocity.x = lerp(velocity.x, 0.0, friction * delta)
+	else:
+		velocity.x = lerp(velocity.x, speed * direction, acceleration * delta)
+	
+	if not is_on_floor() and \
+	velocity.y < max_fall_speed:
+		velocity.y = min(velocity.y + gravity * delta, max_fall_speed)
+	
+	if Input.is_action_just_pressed("JUMP") and \
+	is_on_floor():
+		velocity.y = -jump_strength
+	
+	move_and_slide()
+
+
+func _grid_based_update(_delta: float) -> void:
+	pass
+
+
+func _grid_based_physics_update(_delta: float) -> void:
+	pass
